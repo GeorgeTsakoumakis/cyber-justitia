c6db8c44e057b6e8fe5dd424c39926b17eb61eb0
GeorgeTsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-05-04 15:59:11 +0100

    Create custom user table
    Add phone, flair, description, is_professional, is_banned variables, validator for phone number, and 'users' table name

--- users/models.py
+++ users/models.py
@@ -1,3 +1,27 @@
+from django.core.validators import RegexValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    class Meta:
+        db_table = 'users'
+
+    description = models.TextField(max_length=256, null=True, blank=True,
+                                   help_text="A short description about yourself")
+    flair = models.CharField(max_length=100, null=False, blank=False, help_text="Short text that best describes your "
+                                                                                "professional skills")
+    is_professional = models.BooleanField(default=False)
+    is_banned = models.BooleanField(default=False)
+    phone = models.CharField(max_length=20,
+                             null=True,
+                             blank=True,
+                             validators=[
+                                 RegexValidator(
+                                     regex=r'^\d{4}-\d{3}-\d{4}$',
+                                     message="Phone number must be entered in the format: '9999-999-9999'."
+                                 )
+                             ])
+
+    def __str__(self):
+        return self.username
