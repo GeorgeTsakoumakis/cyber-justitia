2468eec027847b65379fb9918bb07d28db599f4d
Georgios Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-05-14 21:01:11 +0100

    Add CSRF_TRUSTED_ORIGINS for trusted redirect

--- justitia/settings.py
+++ justitia/settings.py
@@ -140,3 +140,5 @@ else:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://127.0.0.1:8000"]
