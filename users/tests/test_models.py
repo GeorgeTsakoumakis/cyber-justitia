62e8d0d0efe6ecd502e3682cb82a7cb1c4a2faa2
PiscotOficial <facaeruvaleriu@gmail.com>
2024-05-18 14:52:00 +0100

    Improve test cases for blank and whitespace names

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -56,23 +56,25 @@ class CustomUserModelTest(TestCase):
 
     def test_blank_names(self):
         # Check that first_name and last_name are not blank
-        with self.assertRaises(ValueError):
-            CustomUser.objects.create_user(
-                username='user2',
-                first_name='',
-                last_name='Lastname',
-                email='user2@example.com',
-                password='password123'
-            )
+        user = CustomUser(
+            username='user2',
+            first_name='',
+            last_name='Lastname',
+            email='user2@example.com',
+            password='password123'
+        )
+        with self.assertRaises(ValidationError):
+            user.full_clean()
 
-        with self.assertRaises(ValueError):
-            CustomUser.objects.create_user(
-                username='user3',
-                first_name='Firstname',
-                last_name='',
-                email='user3@example.com',
-                password='password123'
-            )
+        user = CustomUser(
+            username='user3',
+            first_name='Firstname',
+            last_name='',
+            email='user3@example.com',
+            password='password123'
+        )
+        with self.assertRaises(ValidationError):
+            user.full_clean()
 
     def test_blank_username(self):
         # Check that username is not blank
@@ -171,23 +173,25 @@ class CustomUserModelTest(TestCase):
 
     def test_whitespace_names(self):
         # Check that first_name and last_name are not blank
-        with self.assertRaises(ValueError):
-            CustomUser.objects.create_user(
+        with self.assertRaises(ValidationError):
+            user = CustomUser.objects.create_user(
                 username='user2',
                 first_name='     ',
                 last_name='Lastname',
                 email='user2@example.com',
                 password='password123'
             )
+            user.full_clean()
 
-        with self.assertRaises(ValueError):
-            CustomUser.objects.create_user(
+        with self.assertRaises(ValidationError):
+            user = CustomUser.objects.create_user(
                 username='user3',
                 first_name='Firstname',
                 last_name='     ',
                 email='user3@example.com',
                 password='password123'
             )
+            user.full_clean()
 
     def test_whitespace_email(self):
         with self.assertRaises(ValidationError):
