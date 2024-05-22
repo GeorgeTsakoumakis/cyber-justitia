580d51654f5a9840bf3bbcfa2cc127ffca4040a9
Georgios Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-05-23 00:46:38 +0100

    feat: create Post model and forum url

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils.text import slugify
+from hitcount.models import HitCountMixin, HitCount
+from django.contrib.contenttypes.fields import GenericRelation
+from django.shortcuts import reverse
 
-# Create your models here.
+CustomUser = get_user_model()
+
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+        db_table = "posts"
+
+    post_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256, unique=True, blank=True)
+    text = models.CharField(max_length=40000)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+    votes = models.IntegerField(default=0)
+    hit_count_generic = GenericRelation(
+        HitCount,
+        object_id_field="object_pk",
+        related_query_name="hit_count_generic_relation",
+    )
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return reverse("post_detail", kwargs={"slug": self.slug})
--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .utils import update_views
 
 
 def forums(request):
-    return render(request, "forum.html")
+    posts = Post.objects.all()
+
+    context = {
+        "posts": posts,
+    }
+    return render(request, "forum.html", context)
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    context = {
+        "post": post,
+    }
+    update_views(request, post)
+
+    return render(request, "forumpost.html", context)
