9dd47871fd99a45fdd5835869709924d901c022d
Georgios Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-05-23 16:47:47 +0100

    feat: create comment model

--- forum/models.py
+++ forum/models.py
@@ -69,3 +69,43 @@ class Post(models.Model):
         if len(self.text) > 40000:
             raise ValidationError(_("Text cannot exceed 40000 characters."), code="invalid")
         return cleaned_data
+
+
+class Comment(models.Model):
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+        db_table = "comments"
+
+    comment_id = models.AutoField(primary_key=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    text = models.TextField(max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+    votes = models.IntegerField(default=0)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if not self.text:
+            raise ValidationError(_("Text field is required."), code="invalid")
+        if len(self.text) > 40000:
+            raise ValidationError(_("Text cannot exceed 40000 characters."), code="invalid")
+        return cleaned_data
+
+    def save(self, *args, **kwargs):
+        """
+        Save the comment and perform validation checks before saving
+        """
+        self.full_clean()
+        super(Comment, self).save(*args, **kwargs)
+
+    def delete(self, using=None, keep_parents=False):
+        """
+        Mark the comment as deleted and save it. The text is replaced with [deleted]
+        :return:  Comment object
+        """
+        self.text = "[deleted]"
+        self.is_deleted = True
+        self.save()
+        return self
