9bc7e532a82f9838224714fe2c0af6bcf65eecd4
Georgios Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-05-23 16:35:12 +0100

    fix: add field validation

--- forum/forms.py
+++ forum/forms.py
@@ -7,19 +7,26 @@ class CreatePostForm(forms.ModelForm):
     """
     Form for creating a new post. It includes fields for the title and text of the post.
     """
+
     class Meta:
         model = Post
         fields = ["title", "text"]
+
+    title = forms.CharField(label=_("Title"),
+                            max_length=256,
+                            error_messages={"required": _("Title field is required."),
+                                            "max_length": _("Title cannot exceed 256 characters.")})
+
+    text = forms.CharField(label=_("Text"),
+                           widget=forms.Textarea(attrs={"rows": 10, "cols": 40}),
+                           error_messages={"required": _("Text field is required."),
+                                           "max_length": _("Text cannot exceed 40000 characters.")})
 
     def clean_title(self):
         """
         Check if the title is empty or exceeds allowed character limit.
         """
         title = self.cleaned_data["title"]
-        if not title:
-            raise forms.ValidationError(_("Title field is required."), code="invalid")
-        if len(title) > 256:
-            raise forms.ValidationError(_("Title cannot exceed 256 characters."), code="invalid")
         return title
 
     def clean_text(self):
@@ -27,8 +34,4 @@ class CreatePostForm(forms.ModelForm):
         Check if the text is empty or exceeds allowed character limit.
         """
         text = self.cleaned_data["text"]
-        if not text:
-            raise forms.ValidationError(_("Text field is required."), code="invalid")
-        if len(text) > 40000:
-            raise forms.ValidationError(_("Text cannot exceed 40000 characters."), code="invalid")
         return text
