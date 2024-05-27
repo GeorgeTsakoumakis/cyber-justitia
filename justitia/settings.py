8f31c4c28ede392e33c12750bc11acd6c8485a9b
Georgios Tsakoumakis <g.tsakoumakis2@newcastle.ac.uk>
2024-05-27 21:32:41 +0100

    Specify testing env for db, update .env locally pls

--- justitia/settings.py
+++ justitia/settings.py
@@ -83,22 +83,23 @@ ROOT_URLCONF = "justitia.urls"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 # Postgres
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST"),
-        "PORT": os.getenv("DB_PORT"),
-    }
-}
-if "test" in sys.argv or os.getenv("GITHUB_TEST") == "True":
+if "test" in sys.argv or os.getenv("GITHUB_TEST") == "True" or os.getenv("EXPERIMENTAL") == "True":
     print("Using SQLite for testing")
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT"),
         }
     }
 
