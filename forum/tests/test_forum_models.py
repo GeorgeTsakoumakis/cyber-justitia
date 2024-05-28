ff68cd5959723fcd114040455ba70de582e66edf
PiscotOficial <facaeruvaleriu@gmail.com>
2024-05-28 17:41:50 +0100

    Added docstrings for test_forum_models

--- forum/tests/test_forum_models.py
+++ forum/tests/test_forum_models.py
@@ -8,9 +8,16 @@ from forum.models import Post, Comment, PostVote, CommentVote
 # Get the CustomUser model
 CustomUser = get_user_model()
 
+
 class PostModelTests(TestCase):
+    """
+    Test case for the Post model.
+    """
 
     def setUp(self):
+        """
+        TFM1: Set up a test user for use in the tests.
+        """
         self.user = get_user_model().objects.create_user(
             username='testuser',
             password='Password123!',
@@ -18,6 +25,9 @@ class PostModelTests(TestCase):
         )
 
     def test_create_post_with_valid_data(self):
+        """
+        TFM2: Test creating a post with valid data.
+        """
         post = Post.objects.create(
             title='Valid Title',
             text='Valid text.',
@@ -28,6 +38,9 @@ class PostModelTests(TestCase):
         self.assertEqual(post.user, self.user)
 
     def test_create_post_with_empty_title(self):
+        """
+        TFM3: Test creating a post with an empty title.
+        """
         post = Post(
             title='',
             text='Valid text.',
@@ -37,6 +50,9 @@ class PostModelTests(TestCase):
             post.full_clean()
 
     def test_create_post_with_long_title(self):
+        """
+        TFM4: Test creating a post with a title exceeding the maximum length.
+        """
         long_title = 'a' * 257
         post = Post(
             title=long_title,
@@ -47,6 +63,9 @@ class PostModelTests(TestCase):
             post.full_clean()
 
     def test_create_post_with_empty_text(self):
+        """
+        TFM5: Test creating a post with empty text.
+        """
         post = Post(
             title='Valid Title',
             text='',
@@ -56,6 +75,9 @@ class PostModelTests(TestCase):
             post.full_clean()
 
     def test_create_post_with_long_text(self):
+        """
+        TFM6: Test creating a post with text exceeding the maximum length.
+        """
         long_text = 'a' * 40001
         post = Post(
             title='Valid Title',
@@ -66,6 +88,9 @@ class PostModelTests(TestCase):
             post.full_clean()
 
     def test_post_slug_is_unique(self):
+        """
+        TFM7: Test that each post has a unique slug.
+        """
         post1 = Post.objects.create(
             title='Unique Title',
             text='Valid text.',
@@ -79,6 +104,9 @@ class PostModelTests(TestCase):
         self.assertNotEqual(post1.slug, post2.slug)
 
     def test_post_delete(self):
+        """
+        TFM8: Test deleting a post.
+        """
         post = Post.objects.create(
             title='Title to Delete',
             text='Text to delete.',
@@ -91,8 +119,14 @@ class PostModelTests(TestCase):
 
 
 class CommentModelTests(TestCase):
+    """
+    Test case for the Comment model.
+    """
 
     def setUp(self):
+        """
+        TFM9: Set up a test user and post for use in the tests.
+        """
         self.user = get_user_model().objects.create_user(
             username='testuser',
             password='Password123!',
@@ -105,6 +139,9 @@ class CommentModelTests(TestCase):
         )
 
     def test_create_comment_with_valid_data(self):
+        """
+        TFM10: Test creating a comment with valid data.
+        """
         comment = Comment.objects.create(
             post=self.post,
             user=self.user,
@@ -115,6 +152,9 @@ class CommentModelTests(TestCase):
         self.assertEqual(comment.post, self.post)
 
     def test_create_comment_with_empty_text(self):
+        """
+        TFM11: Test creating a comment with an empty text.
+        """
         comment = Comment(
             post=self.post,
             user=self.user,
@@ -124,6 +164,9 @@ class CommentModelTests(TestCase):
             comment.full_clean()
 
     def test_create_comment_with_long_text(self):
+        """
+        TFM12: Test creating a comment with text exceeding the maximum length.
+        """
         long_text = 'a' * 40001
         comment = Comment(
             post=self.post,
@@ -134,6 +177,9 @@ class CommentModelTests(TestCase):
             comment.full_clean()
 
     def test_comment_delete(self):
+        """
+        TFM13: Test deleting a comment.
+        """
         comment = Comment.objects.create(
             post=self.post,
             user=self.user,
@@ -145,8 +191,14 @@ class CommentModelTests(TestCase):
 
 
 class PostVoteModelTests(TestCase):
+    """
+    Test case for the PostVote model.
+    """
 
     def setUp(self):
+        """
+        TFM14: Set up a test user and post for use in the tests.
+        """
         self.user = get_user_model().objects.create_user(
             username='testuser',
             password='Password123!',
@@ -159,6 +211,9 @@ class PostVoteModelTests(TestCase):
         )
 
     def test_create_postvote_with_valid_data(self):
+        """
+        TFM15: Test creating a PostVote with valid data.
+        """
         postvote = PostVote.objects.create(
             post=self.post,
             user=self.user,
@@ -169,6 +224,9 @@ class PostVoteModelTests(TestCase):
         self.assertTrue(postvote.vote_type)
 
     def test_create_postvote_without_user(self):
+        """
+        TFM16: Test creating a PostVote without specifying a user.
+        """
         postvote = PostVote(
             post=self.post,
             vote_type="up"
@@ -177,6 +235,9 @@ class PostVoteModelTests(TestCase):
             postvote.full_clean()
 
     def test_create_postvote_without_post(self):
+        """
+        TFM17: Test creating a PostVote without specifying a post.
+        """
         postvote = PostVote(
             user=self.user,
             vote_type="up"
@@ -185,6 +246,9 @@ class PostVoteModelTests(TestCase):
             postvote.full_clean()
 
     def test_create_duplicate_postvote(self):
+        """
+        TFM18: Test creating a duplicate PostVote.
+        """
         PostVote.objects.create(
             post=self.post,
             user=self.user,
@@ -200,7 +264,14 @@ class PostVoteModelTests(TestCase):
 
 
 class CommentVoteModelTests(TestCase):
+    """
+    Test case for the CommentVote model.
+    """
+
     def setUp(self):
+        """
+        TFM19: Set up a test user, post, and comment for use in the tests.
+        """
         self.user = get_user_model().objects.create_user(
             username='testuser',
             password='Password123!',
@@ -218,6 +289,9 @@ class CommentVoteModelTests(TestCase):
         )
 
     def test_create_commentvote_with_valid_data(self):
+        """
+        TFM20: Test creating a CommentVote with valid data.
+        """
         commentvote = CommentVote.objects.create(
             comment=self.comment,
             user=self.user,
@@ -228,6 +302,9 @@ class CommentVoteModelTests(TestCase):
         self.assertTrue(commentvote.vote_type)
 
     def test_create_commentvote_without_user(self):
+        """
+        TFM21: Test creating a CommentVote without specifying a user.
+        """
         commentvote = CommentVote(
             comment=self.comment,
             vote_type="up"
@@ -236,6 +313,9 @@ class CommentVoteModelTests(TestCase):
             commentvote.full_clean()
 
     def test_create_commentvote_without_comment(self):
+        """
+        TFM22: Test creating a CommentVote without specifying a comment.
+        """
         commentvote = CommentVote(
             user=self.user,
             vote_type="up"
@@ -244,6 +324,9 @@ class CommentVoteModelTests(TestCase):
             commentvote.full_clean()
 
     def test_create_duplicate_commentvote(self):
+        """
+        TFM23: Test creating a duplicate CommentVote.
+        """
         CommentVote.objects.create(
             comment=self.comment,
             user=self.user,
