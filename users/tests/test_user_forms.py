6992d397cad4ba923d405dc779af06f426d36734
PiscotOficial <facaeruvaleriu@gmail.com>
2024-05-28 17:37:19 +0100

    Added docstrings for test_user_forms

--- users/tests/test_user_forms.py
+++ users/tests/test_user_forms.py
@@ -6,7 +6,14 @@ from users.models import CustomUser, ProfessionalUser
 
 
 class UserFormTests(TestCase):
+    """
+    Test case for user-related forms.
+    """
+
     def setUp(self):
+        """
+        TUF1: Set up a test user and professional user for use in the tests.
+        """
         self.user = CustomUser.objects.create_user(
             username='testuser',
             password='Password123!',
@@ -20,6 +27,9 @@ class UserFormTests(TestCase):
         )
 
     def test_valid_user_details_update(self):
+        """
+        TUF2: Test updating user details with valid data.
+        """
         form_data = {
             'first_name': 'NewFirstName',
             'last_name': 'NewLastName',
@@ -29,6 +39,9 @@ class UserFormTests(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_user_details_update_with_empty_first_name(self):
+        """
+        TUF3: Test updating user details with an empty first name.
+        """
         form_data = {
             'first_name': '',
             'last_name': 'NewLastName',
@@ -39,6 +52,9 @@ class UserFormTests(TestCase):
         self.assertIn('first_name', form.errors)
 
     def test_user_details_update_with_empty_last_name(self):
+        """
+        TUF4: Test updating user details with an empty last name.
+        """
         form_data = {
             'first_name': 'NewFirstName',
             'last_name': '',
@@ -49,6 +65,9 @@ class UserFormTests(TestCase):
         self.assertIn('last_name', form.errors)
 
     def test_user_details_update_with_empty_email(self):
+        """
+        TUF5: Test updating user details with an empty email.
+        """
         form_data = {
             'first_name': 'NewFirstName',
             'last_name': 'NewLastName',
@@ -59,6 +78,9 @@ class UserFormTests(TestCase):
         self.assertIn('email', form.errors)
 
     def test_user_details_update_with_first_name_exceeding_max_length(self):
+        """
+        TUF6: Test updating user details with a first name exceeding max length.
+        """
         form_data = {
             'first_name': 'a' * 151,
             'last_name': 'NewLastName',
@@ -69,6 +91,9 @@ class UserFormTests(TestCase):
         self.assertIn('first_name', form.errors)
 
     def test_user_details_update_with_last_name_exceeding_max_length(self):
+        """
+        TUF7: Test updating user details with a last name exceeding max length.
+        """
         form_data = {
             'first_name': 'NewFirstName',
             'last_name': 'a' * 151,
@@ -79,6 +104,9 @@ class UserFormTests(TestCase):
         self.assertIn('last_name', form.errors)
 
     def test_user_details_update_with_duplicate_email(self):
+        """
+        TUF8: Test updating user details with a duplicate email.
+        """
         CustomUser.objects.create_user(
             username='otheruser',
             password='Password123!',
@@ -95,6 +123,9 @@ class UserFormTests(TestCase):
         self.assertEqual(form.errors['email'], ['This email is already in use.'])
 
     def test_valid_password_update(self):
+        """
+        TUF9: Test updating the password with valid data.
+        """
         form_data = {
             'old_password': 'Password123!',
             'new_password1': 'NewPassword123!',
@@ -104,6 +135,9 @@ class UserFormTests(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_password_update_with_incorrect_old_password(self):
+        """
+        TUF10: Test updating the password with an incorrect old password.
+        """
         form_data = {
             'old_password': 'WrongPassword123!',
             'new_password1': 'NewPassword123!',
@@ -115,6 +149,9 @@ class UserFormTests(TestCase):
         self.assertEqual(form.errors['old_password'], ['The old password is incorrect.'])
 
     def test_password_update_with_non_matching_new_passwords(self):
+        """
+        TUF11: Test updating the password with non-matching new passwords.
+        """
         form_data = {
             'old_password': 'Password123!',
             'new_password1': 'NewPassword123!',
@@ -126,6 +163,9 @@ class UserFormTests(TestCase):
         self.assertEqual(form.errors['new_password2'], ['The new passwords do not match.'])
 
     def test_password_update_with_new_password_same_as_old_password(self):
+        """
+        TUF12: Test updating the password with the new password same as the old password.
+        """
         form_data = {
             'old_password': 'Password123!',
             'new_password1': 'Password123!',
@@ -137,6 +177,9 @@ class UserFormTests(TestCase):
         self.assertEqual(form.errors['new_password1'], ['The new password cannot be the same as the old password.'])
 
     def test_valid_description_update(self):
+        """
+        TUF13: Test updating the description with valid data.
+        """
         form_data = {
             'description': 'This is a new description.'
         }
@@ -144,6 +187,9 @@ class UserFormTests(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_account_deactivation_with_checkbox_checked(self):
+        """
+        TUF14: Test deactivating the account with the checkbox checked.
+        """
         form_data = {
             'deactivate_profile': True
         }
@@ -151,6 +197,9 @@ class UserFormTests(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_account_deactivation_without_checkbox_checked(self):
+        """
+        TUF15: Test deactivating the account without checking the checkbox.
+        """
         form_data = {
             'deactivate_profile': False
         }
@@ -160,6 +209,9 @@ class UserFormTests(TestCase):
         self.assertEqual(form.errors['deactivate_profile'], ['This field is required.'])
 
     def test_valid_flair_update(self):
+        """
+        TUF16: Test updating the flair with valid data.
+        """
         form_data = {
             'flair': 'New Flair'
         }
@@ -167,6 +219,9 @@ class UserFormTests(TestCase):
         self.assertTrue(form.is_valid())
 
     def test_flair_update_with_empty_flair(self):
+        """
+        TUF17: Test updating the flair with an empty value.
+        """
         form_data = {
             'flair': ''
         }
@@ -175,6 +230,9 @@ class UserFormTests(TestCase):
         self.assertIn('flair', form.errors)
 
     def test_flair_update_with_flair_exceeding_max_length(self):
+        """
+        TUF18: Test updating the flair with a value exceeding max length.
+        """
         form_data = {
             'flair': 'a' * 101
         }
